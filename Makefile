5ece2e5684cfa1b15adc9a0ce2046abeb567d3aa
manu <manuel.deneu@nokia-bell-labs.com>
2021-05-09 17:44:27 +0200

    sim: increase (and count) avail. memory

--- Makefile
+++ Makefile
@@ -52,4 +52,4 @@ clean:
 
 sim:
 	qemu-system-x86_64  -cpu Nehalem,-vme,+pdpe1gb,-xsave,-xsaveopt,-xsavec,-fsgsbase,-invpcid,enforce \
-	-nographic -serial mon:stdio -kernel $(kernelFile) -initrd $(rootServer)
+	-nographic -m 256 -serial mon:stdio -kernel $(kernelFile) -initrd $(rootServer)
--- userland/main.c
+++ userland/main.c
@@ -26,8 +26,25 @@ void __sel4_start_root(void* bootinfoPtr)
     
     seL4_BootInfo* bootInfo = bootinfoPtr; 
 
-    printf_("Hello world\n\n");
+    printf_("Hello world\n");
+
+    printf_("untypeds are from %zi to %zi\n", bootInfo->untyped.start , bootInfo->untyped.end);
     seL4_DebugDumpScheduler();
+
+    unsigned long long total = 0;
+    for (int i=0;i <CONFIG_MAX_NUM_BOOTINFO_UNTYPED_CAPS;i++)
+    {
+        seL4_UntypedDesc untyped = bootInfo->untypedList[i];
+        if(untyped.sizeBits)
+        {
+            printf_("%i untyped size %zi k isdev:%u\n",i, (1 << untyped.sizeBits)/1024, untyped.isDevice);
+            if(!untyped.isDevice)
+            {
+                total += (1 << untyped.sizeBits)/1024;
+            }
+        }
+    }
+    printf_("Total mem %zi k\n", total);
     while (1)
     {
         /* code */
