d9894b7293acabfc4159a4c0c4147af69f49f44d
manu88 <manuel.deneu@gmail.com>
2021-12-04 14:13:39 +0100

    update vector

--- userland/lib/vector.hpp
+++ userland/lib/vector.hpp
@@ -1,14 +1,105 @@
 #pragma once
+#include <cstdlib>
 #include <stddef.h>
+#ifdef UNIT_TESTS
+#include <cassert>
+#else
+#include "../kmalloc.hpp"
+#include "../runtime.h"
+#endif
 
-template <typename Type> class vector {
+struct Allocator {
+  Allocator() {}
+  virtual void *alloc(size_t size) const = 0;
+  virtual void free(void *ptr) const = 0;
+};
+
+#ifdef UNIT_TESTS
+struct Mallocator {
+  void *alloc(size_t size) const { return malloc(size); }
+  void free(void *ptr) const { ::free(ptr); }
+  void *realloc(void *ptr, size_t size) const { return ::realloc(ptr, size); };
+};
+using DefaultAllocator = Mallocator;
+#else
+struct KMallocator {
+  void *alloc(size_t size) const { return kmalloc(size); }
+  void free(void *ptr) const { ::kfree(ptr); }
+  void *realloc(void *ptr, size_t size) const { return ::realloc(ptr, size); };
+};
+using DefaultAllocator = KMallocator;
+#endif
+
+template <class Type> struct Iterator {
+
+  Iterator(Type *i) : _item(i) {}
+  Type *_item;
+  Iterator operator++() {
+    _item += 1;
+    return *this;
+  }
+  bool operator!=(const Iterator &rhs) { return _item != rhs._item; }
+
+  Type &operator*() const { return *_item; }
+};
+
+template <typename Type, class AllocatorType = DefaultAllocator> class vector {
 public:
   typedef const Type &const_reference;
+  typedef Iterator<Type> iterator;
+  typedef const Iterator<Type> const_iterator;
+  vector(const AllocatorType &allocator = DefaultAllocator())
+      : _allocator(allocator) {}
+  vector(size_t count, const AllocatorType &allocator = DefaultAllocator())
+      : vector(allocator) {
+    _reservation = count;
+  }
+
+  ~vector() {
+    if (_data) {
+      _allocator.free(_data);
+    }
+  }
+  [[nodiscard]] constexpr bool empty() const noexcept { return _size == 0; }
 
   constexpr size_t size() const noexcept { return _size; }
 
-  constexpr void push_back(Type &&value) {}
+  constexpr void push_back(const Type &value) {
+    doRealloc();
+    _data[_size++] = value;
+  }
+
+  constexpr void push_back(Type &&value) {
+    doRealloc();
+    _data[_size++] = value;
+  }
+  constexpr void clear() noexcept { _size = 0; }
+  constexpr const_reference operator[](size_t pos) const { return _data[pos]; }
+
+  constexpr const_iterator begin() noexcept { return const_iterator(_data); };
+  constexpr const_iterator end() noexcept {
+    return const_iterator(_data + _size);
+  };
 
 private:
+  void doRealloc() {
+    if (!_data) {
+      if (_reservation == 0) {
+        _reservation = 10;
+      }
+      auto sizeToAlloc = _reservation * sizeof(Type);
+      _data = reinterpret_cast<Type *>(_allocator.alloc(sizeToAlloc));
+      assert(_data);
+    }
+    if (_size >= _reservation) {
+      _reservation += 10;
+      auto sizeToRealloc = _reservation * sizeof(Type);
+      _data =
+          reinterpret_cast<Type *>(_allocator.realloc(_data, sizeToRealloc));
+    }
+  }
+  const AllocatorType &_allocator;
   size_t _size = 0;
+  size_t _reservation = 0;
+  Type *_data = nullptr;
 };
--- userland/unittests/test_vector.cpp
+++ userland/unittests/test_vector.cpp
@@ -1,14 +1,39 @@
 #include "tests.hpp"
 #include "vector.hpp"
 #include <assert.h>
+#include <iostream>
 #include <stdio.h>
 
 int testVector() {
-  printf("Test vector\n");
+  printf("Test vector 1\n");
   vector<bool> a;
   assert(a.size() == 0);
+  assert(a.empty());
   a.push_back(true);
-  assert(a.size() == 1);
+  a.push_back(false);
+  a.push_back(true);
+  for (const auto &e : a) {
+    std::cout << "Elem " << e << "\n";
+  }
 
+  assert(a.size() == 3);
+  assert(a[0] == true);
+  a.clear();
+  assert(a.empty());
+  assert(a.size() == 0);
+  printf("Test vector 2\n");
+
+  vector<int> b;
+  for (int i = 0; i < 100; i++) {
+    b.push_back(i);
+  }
+  assert(b.size() == 100);
+  int i = 0;
+  for (const auto &j : b) {
+    assert(i == j);
+    i++;
+  }
+  b.clear();
+  assert(b.empty());
   return 0;
 }
