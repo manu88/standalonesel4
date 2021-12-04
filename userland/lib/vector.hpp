8f547aa2984eb3cce58fcdf516023a239763e189
manu88 <manuel.deneu@gmail.com>
2021-12-04 16:48:03 +0100

    vector and string use kmalloc et all

--- userland/lib/vector.hpp
+++ userland/lib/vector.hpp
@@ -1,33 +1,14 @@
 #pragma once
-#include <cstdlib>
 #include <stddef.h>
 #ifdef UNIT_TESTS
 #include <cassert>
+#include <cstdlib>
+#define kmalloc malloc
+#define krealloc realloc
+#define kfree free
 #else
 #include "../kmalloc.hpp"
 #include "../runtime.h"
-#endif
-
-struct Allocator {
-  Allocator() {}
-  virtual void *alloc(size_t size) const = 0;
-  virtual void free(void *ptr) const = 0;
-};
-
-#ifdef UNIT_TESTS
-struct Mallocator {
-  void *alloc(size_t size) const { return malloc(size); }
-  void free(void *ptr) const { ::free(ptr); }
-  void *realloc(void *ptr, size_t size) const { return ::realloc(ptr, size); };
-};
-using DefaultAllocator = Mallocator;
-#else
-struct KMallocator {
-  void *alloc(size_t size) const { return kmalloc(size); }
-  void free(void *ptr) const { ::kfree(ptr); }
-  void *realloc(void *ptr, size_t size) const { return ::realloc(ptr, size); };
-};
-using DefaultAllocator = KMallocator;
 #endif
 
 template <class Type> struct Iterator {
@@ -43,21 +24,17 @@ template <class Type> struct Iterator {
   Type &operator*() const { return *_item; }
 };
 
-template <typename Type, class AllocatorType = DefaultAllocator> class vector {
+template <typename Type> class vector {
 public:
   typedef const Type &const_reference;
   typedef Iterator<Type> iterator;
   typedef const Iterator<Type> const_iterator;
-  vector(const AllocatorType &allocator = DefaultAllocator())
-      : _allocator(allocator) {}
-  vector(size_t count, const AllocatorType &allocator = DefaultAllocator())
-      : vector(allocator) {
-    _reservation = count;
-  }
+  vector() {}
+  vector(size_t count) : vector() { _reservation = count; }
 
   ~vector() {
     if (_data) {
-      _allocator.free(_data);
+      kfree(_data);
     }
   }
   [[nodiscard]] constexpr bool empty() const noexcept { return _size == 0; }
@@ -76,6 +53,9 @@ public:
   constexpr void clear() noexcept { _size = 0; }
   constexpr const_reference operator[](size_t pos) const { return _data[pos]; }
 
+  constexpr const_reference front() const { return _data[0]; }
+  constexpr const_reference back() const { return _data[_size - 1]; }
+
   constexpr const_iterator begin() noexcept { return const_iterator(_data); };
   constexpr const_iterator end() noexcept {
     return const_iterator(_data + _size);
@@ -83,22 +63,16 @@ public:
 
 private:
   void doRealloc() {
-    if (!_data) {
+    if (_data == nullptr || _size >= _reservation) {
       if (_reservation == 0) {
         _reservation = 10;
+      } else {
+        _reservation = _size * 2;
       }
-      auto sizeToAlloc = _reservation * sizeof(Type);
-      _data = reinterpret_cast<Type *>(_allocator.alloc(sizeToAlloc));
-      assert(_data);
-    }
-    if (_size >= _reservation) {
-      _reservation += 10;
       auto sizeToRealloc = _reservation * sizeof(Type);
-      _data =
-          reinterpret_cast<Type *>(_allocator.realloc(_data, sizeToRealloc));
+      _data = reinterpret_cast<Type *>(krealloc(_data, sizeToRealloc));
     }
   }
-  const AllocatorType &_allocator;
   size_t _size = 0;
   size_t _reservation = 0;
   Type *_data = nullptr;
