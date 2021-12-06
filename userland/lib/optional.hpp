ce2125090082e7db40ed2b4233834633d8a9bbf8
manu88 <manuel.deneu@gmail.com>
2021-12-06 09:53:00 +0100

    add Optional

---
+++ userland/lib/optional.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+template <typename T>
+class Optional {
+public:
+    constexpr Optional() noexcept: _has_value(false){}
+    explicit Optional(T value): value_(value), _has_value(true) {}
+    ~Optional(){}
+    constexpr bool has_value() const noexcept{
+        
+        return _has_value;
+    }
+    constexpr const T& operator*() const& noexcept{
+        return value_;
+    }
+    constexpr T& operator*() & noexcept{
+        return value_;
+    }
+private:
+    struct empty_byte {};
+    union {
+        empty_byte empty_;
+        T value_;
+    };
+    bool _has_value;
+};
---
+++ userland/unittests/test_optional.cpp
@@ -0,0 +1,25 @@
+#include "tests.hpp"
+#include "optional.hpp"
+#include <assert.h>
+
+Optional<int> create(int value){
+    return Optional<int>(value);
+}
+
+int testOptional(){
+    Optional<bool> a = {};
+    assert(a.has_value() == false);
+
+
+    auto b = Optional<bool>(true);
+    assert(b.has_value());
+    assert(*b == true);
+
+
+    auto c = create(42);
+    assert(c.has_value());
+    assert(*c == 42);
+
+
+    return 0;
+}
