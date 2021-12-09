5a0e9acaba32ffa152dd575a58f6c6ba1775b8bf
manu88 <manuel.deneu@gmail.com>
2021-12-09 08:09:58 +0100

    add memcmp and memmove

--- userland/cstubs.c
+++ userland/cstubs.c
@@ -1,5 +1,6 @@
 #include "runtime.h"
 #include "sel4.hpp"
+#include <stdlib.h>
 
 int __errno_location = 0;
 // TODO: actual implementation :)
@@ -29,4 +30,54 @@ extern "C" char *strcpy(char *dest,  char const*src)
     char *save = dest;
     while((*dest++ = *src++));
     return save;
+}
+
+extern "C" void *memcpy(void *dest, const void *src, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    ((char *)dest)[i] = ((char *)src)[i];
+  }
+  return dest;
+}
+
+extern "C"  int memcmp(const void *str1, const void *str2, size_t count){
+  const unsigned char *s1 = (const unsigned char*)str1;
+  const unsigned char *s2 = (const unsigned char*)str2;
+  while (count-- > 0)
+    {
+      if (*s1++ != *s2++)
+          return s1[-1] < s2[-1] ? -1 : 1;
+    }
+  return 0;
+}
+
+
+extern "C" void *memmove(void *dest, const void *src, size_t n)
+{
+	char* from = (char*) src;
+	char* to = (char*) dest;
+
+	if (from == to || n == 0)
+		return dest;
+	if (to > from && to-from < (int)n) {
+		/* to overlaps with from */
+		/*  <from......>         */
+		/*         <to........>  */
+		/* copy in reverse, to avoid overwriting from */
+		int i;
+		for(i=n-1; i>=0; i--)
+			to[i] = from[i];
+		return dest;
+	}
+	if (from > to && from-to < (int)n) {
+		/* to overlaps with from */
+		/*        <from......>   */
+		/*  <to........>         */
+		/* copy forwards, to avoid overwriting from */
+		size_t i;
+		for(i=0; i<n; i++)
+			to[i] = from[i];
+		return dest;
+	}
+	memcpy(dest, src, n);
+	return dest;
 }
