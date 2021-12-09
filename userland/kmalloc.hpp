a58f6aa8faf0f2d8719ff52ba7ff6739f0e24108
manu88 <manuel.deneu@gmail.com>
2021-12-09 08:15:44 +0100

    protect kmalloc/kfree with mutex

--- userland/kmalloc.hpp
+++ userland/kmalloc.hpp
@@ -1,8 +1,9 @@
 #pragma once
+#include "sel4.hpp"
 #include <stddef.h>
 
 extern "C" {
-void setMemoryPool(void *start, size_t size);
+void setMemoryPool(void *start, size_t size, seL4_CPtr mutexNotif);
 size_t getTotalKMallocated(void);
 void *kmalloc(size_t size);
 void *krealloc(void *ptr, size_t size);
