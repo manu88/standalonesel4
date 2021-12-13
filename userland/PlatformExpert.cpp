677a129c026297f1a05c1c4f2038913573b1639c
manu88 <manuel.deneu@gmail.com>
2021-12-13 10:06:35 +0100

    add platformexpert

---
+++ userland/PlatformExpert.cpp
@@ -0,0 +1,7 @@
+#include "PlatformExpert.hpp"
+#include "runtime.h"
+
+bool PlatformExpert::init(){
+    printf("PlatformExpert::init\n");
+    return true;
+}
---
+++ userland/PlatformExpert.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+class PlatformExpert{
+public:
+    bool init();
+};
