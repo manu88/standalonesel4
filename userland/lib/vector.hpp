c44706214c2d962cbad2740084d4461f830ce8ea
manu88 <manuel.deneu@gmail.com>
2021-12-21 11:11:47 +0100

    vector: don't use krealloc but kmalloc/kfree instead

--- userland/lib/vector.hpp
+++ userland/lib/vector.hpp
@@ -132,7 +132,10 @@ private:
         _reservation = _size * 2;
       }
       auto sizeToRealloc = _reservation * sizeof(Type);
-      _data = reinterpret_cast<Type *>(krealloc(_data, sizeToRealloc));
+      auto newData = reinterpret_cast<Type *>(kmalloc(sizeToRealloc));
+      memcpy(newData, _data, _size*sizeof(Type));
+      kfree(_data);
+      _data = newData;
     }
   }
   size_t _size = 0;
